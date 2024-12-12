f548fcf73c0f238181fc919c40a106423e2cc4d7
mwku <168341474+mwku@users.noreply.github.com>
2024-12-12 13:54:35 +0000

    main

---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+document.activeElement('DOMContentLoaded',function (){
+    window.location.href = 'https://www.google.com/search?q=rickroll&rlz=1C1GCEU_zh-TW&oq=rickroll&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg70gEIMjQ1M2owajeoAgiwAgE&sourceid=chrome&ie=UTF-8#'
+})
