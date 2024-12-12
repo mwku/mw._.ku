2029b71db81987e84e72e6fcba078e4f21527564
mwku <kumingwei0818@gmail.com>
2024-12-12 22:01:24 +0800

    debug

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,3 @@
-document.activeElement('DOMContentLoaded',function (){
-    window.location.href = 'https://www.google.com/search?q=rickroll&rlz=1C1GCEU_zh-TW&oq=rickroll&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg70gEIMjQ1M2owajeoAgiwAgE&sourceid=chrome&ie=UTF-8#'
+document.addEventListener('DOMContentLoaded', function () {
+    window.location.href = 'https://m.youtube.com/watch?v=dQw4w9WgXcQ'
 })
